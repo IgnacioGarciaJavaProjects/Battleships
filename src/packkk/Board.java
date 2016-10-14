a645185d548a0a86f5db81aaf723e423b7cb052e
IgnacioGarcia198 <ignaciogarcia198@gmail.com>
2016-10-14 21:22:28 +0200

    Tidying up the classes Board and PlayerBoard.
    We also need to make a different structure in Board to hold the ship
    coordinates.

--- src/packkk/Board.java
+++ src/packkk/Board.java
@@ -174,74 +174,7 @@ public class Board {
 		}
 	}*/
 	
-	int chooseDirection(int sq) {
-		int dChosen = -1; 
-		do {
-		    int row = posToRow(sq);
-			int col = posToCol(sq);
-		    dChosen = extractPos(r.nextInt(nAtadir), attackDirections, nAtadir);
-			nAtadir --;
-			
-			switch(dChosen) {
-				case 0:
-					// we need to take into account first if there are 
-					// short ships left, in order to set the max distance
-					// from the borders of the board where to look.
-					// idea: variable shortestShipAlive.
-					// so if row < 0 + shortestShipAlive-1 then we must
-					// delete that direction from the attackdirections.
-					// same if row > height - shortestShipAlive.
-					if(row < shortestShipAlive) {			
-						continue;
-					}
-					break;
-				case 1: 
-					if(row < shortestShipAlive || col > width - shortestShipAlive) {
-						continue;
-					}
-					break;
-					
-				case 2: 
-					if(col > width - shortestShipAlive) {
-						continue;
-					}
-					break;
-					
-				case 3: 
-					if(row > height - shortestShipAlive || col > width - shortestShipAlive) {
-						continue;
-					}
-					break;
-					
-				case 4: 
-					if(row > height - shortestShipAlive) {
-						continue;
-					}
-					break;
-					
-				case 5: 
-					if(row > height - shortestShipAlive || col < shortestShipAlive) {
-						continue;
-					}
-					break;
-					
-				case 6: 
-					if(col < shortestShipAlive) {
-						continue;
-					}
-					break;
-					
-				case 7: 
-					if(row < shortestShipAlive || col < shortestShipAlive) {
-						continue;
-					}
-					break;
-				
-			}
-			
-		} while(nAtadir > 0);
-		return dChosen;
-	}
+	
 	
 	void deletePos(int pos, int[] t, int limit) {
 		while(pos < limit) {
